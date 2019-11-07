b157d939c21b34f3d0bd4b158d4754a4387480ad
utrow <buchitaro@live.jp>
2019-11-08 01:01:12 +0900

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const inputNumber = process.argv.slice(2)[0]
+if (!inputNumber || isNaN(inputNumber) || inputNumber <= 0) {
+    console.log("Missing argment.");
+    return
+}
+
+fitEightNumbers(inputNumber)
+
+function fitEightNumbers(number) {
+    const multiple = Math.floor(inputNumber / 8)
+    const relatedSideCount = 2
+
+    const isMinimum = multiple - relatedSideCount < 0
+    const startIndex = isMinimum ? 0 : multiple - relatedSideCount
+
+    const eightNumbers = Array(relatedSideCount * 2 + 1).fill().map((_, i) => (startIndex + i) * 8);
+    console.log(eightNumbers.join(' '))
+}
